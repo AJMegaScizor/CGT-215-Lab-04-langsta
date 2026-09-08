7d426246cd695f40e6141d014a2515723a4f0186
AJMegaScizor <ajlangston06@gmail.com>
2026-09-08 16:13:55 -0400

    trying

--- CGT-215-Lab-04-langsta/CGT-215-Lab-04-langsta.cpp
+++ CGT-215-Lab-04-langsta/CGT-215-Lab-04-langsta.cpp
@@ -1,20 +1,57 @@
-// CGT-215-Lab-04-langsta.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+// Print out the menu of choices for the user to select from
+void printMenu() {
+	cout << "Please Select which operation to perform:" << endl;
+	cout << "\t1. Factorial" << endl;
+	cout << "\t2. Arithmetic Series" << endl;
+	cout << "\t3. Geometric Series" << endl;
+	cout << "\t4. Exit" << endl;
+	cout << "Your Selection: ";
+}
+void factorial(int n, int*f) {
+	int i;
+	cout << "Factorial:" << endl;
+	cout << "Enter a number please:";
+	cin >> n;
+	while (n < 0) {
+		cout << "Enter a positive number please!" << endl;
+		cin >> n;
+	}
+	cout << " Your Selection:" << n << endl;
+	for (i = n; i >= 1; i--)
+		*f = (*f) * i;
+	cout<< i << endl;
+}
+void arithmetic() {
+	// I recommend writing your arithmetic series code here
+}
+void geometric() {
+	// I recommend writing your geometric series code here
+}
+int main() {
+	int choice;
+	char again;
+
+	do {
+		printMenu();
+		cin >> choice;
+		// Quit if user chooses to exit (or any invalid choice)
+		if (choice > 3 || choice < 1) {
+			return 0;
+		}
+		else if (choice == 1) {
+			factorial();
+		}
+		else if (choice == 2) {
+			arithmetic();
+		}
+		else if (choice == 3) {
+			geometric();
+		}
+		cout << "Go Again? [Y/N] ";
+		cin >> again;
+	} while (again == 'y' || again == 'Y');
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
